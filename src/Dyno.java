c842e2ff5a795261ea7873c4e29e4668fcbfc35f
Austin Theriault <austin@cutedogs.org>
2017-10-09 16:51:08 -0400

    4 spaces please

--- src/Dyno.java
+++ src/Dyno.java
@@ -16,7 +16,7 @@ public class Dyno {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] choices = {"Petris"};
+        String[] choices = {"Petris"};
 		JOptionPane.showInputDialog(null, "Game Selection","Choose a game.", JOptionPane.QUESTION_MESSAGE, null,choices,choices[0]);
 	}
 
